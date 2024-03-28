d672369b4d56d3a521c5539f8578422d6e506a2d
simcodes <segunmichael24@gmail.com>
2024-03-28 03:45:47 +0100

    link to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link"
+
 export default function Home() {
-  return <p>Home Page</p>
+  return <main>
+    <h1 className={`mb-4 text-xl md:text-4xl font-bold`}>
+        Welcome to MedCare
+      </h1>
+      <div className="flex">
+      <Link href="/dashboard" className='text-cyan-500 me-2'>
+        Dashboard
+      </Link>
+      <Link href="/login" className='text-cyan-500'>
+        Login
+      </Link>
+    </div>
+  </main>
 }
